fd6ef0e1c73c099bb9ee9cfa26ccbcbdee8310e4
welangaieric <welangaieric@gmail.com>
2024-02-04 18:08:24 +0300

    changed package.json

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const mysql = require('mysql2');
+const cors = require('cors')
+const morgan = require('morgan')
+// const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,8 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
+app.use(morgan('dev'))
 
 const db = mysql.createConnection({
   host: 'localhost',
